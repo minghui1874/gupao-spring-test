2bf2f51ae39b6b896759bc6f0ac8d6ea27831169
minghu <minghui@klicen.com>
2020-03-16 23:31:14 +0800

    提交

--- src/main/java/com/gupao/spring/framework/context/support/GPAbstractApplicationContext.java
+++ src/main/java/com/gupao/spring/framework/context/support/GPAbstractApplicationContext.java
@@ -6,8 +6,7 @@ package com.gupao.spring.framework.context.support;
  */
 public abstract class GPAbstractApplicationContext {
 
-    // 受保护，只提供给子类重写
-    protected void refresh(){
+    public void refresh(){
 
     }
 
